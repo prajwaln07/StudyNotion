e6386d3603705914a5f452a8f28979b16dde1a5e
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2024-09-11 00:01:47 +0530

    done with part  2

--- models/User.js
+++ models/User.js
@@ -47,6 +47,13 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
+
+		token:{
+			type:String
+		},
+		resetPasswordExpires:{
+			type:String,
+		},
 		courseProgress: [
 			{
 				type: mongoose.Schema.Types.ObjectId,
